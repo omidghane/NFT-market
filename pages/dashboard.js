6934c1dc832d8a455f56e7f03d4b1287a70d6a92
Ali Pashmgaran <apg.ali2019@gmail.com>
2023-02-27 21:10:41 +0330

    new style dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -47,22 +47,41 @@ export default function CreatorDashboard() {
   }
   if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>)
   return (
-    <div>
-      <div className="p-4">
-        <h2 className="text-2xl py-2">Items Listed</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} className="rounded" />
-                <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
-                </div>
-              </div>
-            ))
-          }
+    // <div>
+    //   <div className="p-4">
+    //     <h2 className="text-2xl py-2">Items Listed</h2>
+    //       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+    //       {
+    //         nfts.map((nft, i) => (
+    //           <div key={i} className="border shadow rounded-xl overflow-hidden">
+    //             <img src={nft.image} className="rounded" />
+    //             <div className="p-4 bg-black">
+    //               <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+    //             </div>
+    //           </div>
+    //         ))
+    //       }
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="bg-gray-900 text-white">
+  <div className="container mx-auto px-4 py-8">
+    <h1 className="text-3xl font-bold">داشبورد</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-8">
+      {nfts.map((nft, i) => (
+        <div key={i} className="bg-gray-700 rounded-xl overflow-hidden">
+          <img src={nft.image} className="w-full h-64 object-cover" />
+          <div className="p-4">
+            <p className="text-lg font-bold">{nft.title}</p>
+            <p className="text-gray-300">{nft.description}</p>
+            <p className="text-yellow-300 mt-4">{nft.price} ETH</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
+  </div>
+</div>
+
   )
 }
