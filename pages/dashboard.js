f8743891e83dd6112c87c2f1f53b2b6dd5cb9b91
Ali.APG <103386702+aliapg2019@users.noreply.github.com>
2023-05-06 12:33:03 +0330

    remove comments codes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -47,23 +47,6 @@ export default function CreatorDashboard() {
   }
   if (loadingState === 'loaded' && !nfts.length) return (<><h1 className="py-96 px-20 text-3xl bg-gray-900 text-white">No NFTs listed</h1>  </>)
   return (
-    // <div>
-    //   <div className="p-4">
-    //     <h2 className="text-2xl py-2">Items Listed</h2>
-    //       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-    //       {
-    //         nfts.map((nft, i) => (
-    //           <div key={i} className="border shadow rounded-xl overflow-hidden">
-    //             <img src={nft.image} className="rounded" />
-    //             <div className="p-4 bg-black">
-    //               <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
-    //             </div>
-    //           </div>
-    //         ))
-    //       }
-    //     </div>
-    //   </div>
-    // </div>
 
     <div className="bg-gray-900 text-white">
   <div className="container mx-auto px-4 py-8">
