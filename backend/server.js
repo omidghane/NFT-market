4e128a38ef12d87efdb6227df69939dbcea927ff
ali pashmgaran <apg.ali2019@gmail.com>
2023-07-26 01:24:06 +0330

    create api for create-nft

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 const User = require("../models/users");
 const bcrypt = require("bcryptjs");
 
-const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'admin';
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "admin";
 
 const mongooseOptions = {
   useNewUrlParser: true,
@@ -16,54 +16,54 @@ const mongooseOptions = {
 async function connectToDatabase() {
   try {
     await mongoose.connect(`${url}/${dbName}`, mongooseOptions);
-    console.log('Connected successfully to MongoDB server');
+    console.log("Connected successfully to MongoDB server");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 }
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json());
 
 app.post("/register", async (req, res) => {
   console.log(req.body);
-  const { username, password: plainTextPassword, email , wallet } = req.body;
+  const { username, password: plainTextPassword, email, wallet } = req.body;
   const password = await bcrypt.hash(plainTextPassword, 10);
-  
-  if(!username || typeof username !== 'string'){
-    return res.json({
-      status: 'error',
-      error: 'Invalid username'
-    });
-  } 
-  if(!plainTextPassword || typeof plainTextPassword !== 'string'){
-    return res.json({
-      status: 'error',
-      error: 'Invalid password'
-    })
-  }
-  if(plainTextPassword.length < 5){
-    return res.json({
-      status: 'error',
-      error: 'Password too small. Should be at least 6 characters.'
-    })
-  }
 
-  
+  if (!username || typeof username !== "string") {
+    return res.json({
+      status: "error",
+      error: "Invalid username",
+    });
+  }
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({
+      status: "error",
+      error: "Invalid password",
+    });
+  }
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: "error",
+      error: "Password too small. Should be at least 6 characters.",
+    });
+  }
 
   try {
     // Check if the username or email already exists in the database
     const existingUser = await User.findOne({
-      $or: [{ username }, { email } , { wallet }],
+      $or: [{ username }, { email }, { wallet }],
     });
 
     if (existingUser) {
       return res.json({
-        status: 'error',
+        status: "error",
         error: "Username or email or wallet already exists",
       });
     }
@@ -73,23 +73,27 @@ app.post("/register", async (req, res) => {
       username,
       email,
       password,
-      wallet
+      wallet,
     });
 
     console.log("User created successfully:", newUser);
-    res.status(200).json({ 
+    res.status(200).json({
       message: "User registered successfully",
-      status: 'ok'
+      status: "ok",
     });
   } catch (error) {
     console.log(error);
     res.json({
-      status: 'error',
-      error: "An error occurred while registering user"
+      status: "error",
+      error: "An error occurred while registering user",
     });
-  } 
+  }
 });
 
+app.post("/createNft", async (req, res) => {
+  console.log("ok");
+  console.log(req.body);
+});
 async function startServer() {
   await connectToDatabase();
   app.listen(2000, () => {
