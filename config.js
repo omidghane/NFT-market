f63cc928a32c3562ba96552a297c2dd3d9fc4769
omidghane <omidjt2015@gmail.com>
2025-08-18 23:08:24 +0330

    add baseurl

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 
   export const marketplaceAddress = "0x0EA96e2E63e49591024658509cFd0fb380F0F261"
   
+  const config = {
+    baseURL: "http://62.60.198.61:8080", // Global base URL for API requests
+  };
+  export default config;
