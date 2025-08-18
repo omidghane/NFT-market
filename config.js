a7149a3b3b8f20f295588a0a575a132a5a7e97d2
omidghane <omidjt2015@gmail.com>
2025-08-18 23:04:08 +0330

    deployed network

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
 
-export const marketplaceAddress = "0x543a338227dD080102697514978ad0a96dCD65C9";
-
-const config = {
-  baseURL: "http://62.60.198.61:8080", // Global base URL for API requests
-};
-export default config;
+  export const marketplaceAddress = "0x0EA96e2E63e49591024658509cFd0fb380F0F261"
+  
