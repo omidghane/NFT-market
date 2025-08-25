9894b4035d40521d92d758df5635484c82e5490a
omidghane <omidjt2015@gmail.com>
2025-08-25 21:09:07 +0330

    change ganache url

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
 //  unused configuration commented out for now
  ganach: {
   //  url: "https://rushmarketgame.sbs",
-   url: "http://62.60.198.61:8545",
+   url: "ganache.rushmarketgame.sbs",
    accounts: ['0xd368dab76224dc07f32429f05715ba3cdceac25e21fb8996e82f67472126a0fc']
  }
   },
